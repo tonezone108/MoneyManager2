b7dc32fdfda73ab82cf5b42f7e8811650e30ebd7
tonezone108 <tonezone108@gmail.com>
2020-05-29 20:16:07 -0500

    One more try

--- Server/index.js
+++ Server/index.js
@@ -14,6 +14,7 @@ app.use(express.static("build"));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Content-Type", "application/json");
   res.header(
     "Access-Control-Allow-Headers",
